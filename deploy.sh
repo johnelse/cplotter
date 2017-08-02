7e6e272fbc86644d83f11cac18f550721d3e494a
John Else <john.else@gmail.com>
2017-08-02 01:19:22 +0100

    Update deploy script to add the commit SHA to the commit message
    Signed-off-by: John Else <john.else@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,13 @@ git checkout master
 make
 cp -r page page_tmp
 
+COMMIT=`git rev-parse --short master`
+
 # Commit and push changes
 git checkout gh-pages
 mv -f page_tmp/* .
 rmdir page_tmp
-git commit -as -m "Deploy new page"
+git commit -as -m "Deploy new page from $COMMIT"
 git push johnelse gh-pages
 
 git checkout master
